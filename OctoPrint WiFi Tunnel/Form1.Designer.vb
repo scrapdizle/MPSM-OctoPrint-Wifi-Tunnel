6c7f123aae090aefe421df4324444aa6d97077bc
scrapdizle <yourhollisterdude@gmail.com>
2017-10-30 18:11:48 -0400

    Add files via upload

---
+++ OctoPrint WiFi Tunnel/Form1.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tbLog = New System.Windows.Forms.TextBox()
+        Me.tbPrinterIP = New System.Windows.Forms.TextBox()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.pbrFileUploadProgress = New System.Windows.Forms.ProgressBar()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnOctoPrint = New System.Windows.Forms.Button()
+        Me.btnYawam = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.SettingsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.cbComPort = New System.Windows.Forms.ComboBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tbLog
+        '
+        Me.tbLog.BackColor = System.Drawing.SystemColors.Window
+        Me.tbLog.ForeColor = System.Drawing.SystemColors.InfoText
+        Me.tbLog.Location = New System.Drawing.Point(12, 109)
+        Me.tbLog.Multiline = True
+        Me.tbLog.Name = "tbLog"
+        Me.tbLog.ReadOnly = True
+        Me.tbLog.Size = New System.Drawing.Size(353, 99)
+        Me.tbLog.TabIndex = 0
+        '
+        'tbPrinterIP
+        '
+        Me.tbPrinterIP.BackColor = System.Drawing.SystemColors.Window
+        Me.tbPrinterIP.Location = New System.Drawing.Point(12, 54)
+        Me.tbPrinterIP.Name = "tbPrinterIP"
+        Me.tbPrinterIP.Size = New System.Drawing.Size(96, 20)
+        Me.tbPrinterIP.TabIndex = 2
+        Me.tbPrinterIP.Text = "0.0.0.0"
+        Me.tbPrinterIP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(198, 51)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(60, 23)
+        Me.btnConnect.TabIndex = 4
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(264, 51)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(101, 23)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Upload GCODE"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'pbrFileUploadProgress
+        '
+        Me.pbrFileUploadProgress.Location = New System.Drawing.Point(264, 80)
+        Me.pbrFileUploadProgress.Name = "pbrFileUploadProgress"
+        Me.pbrFileUploadProgress.Size = New System.Drawing.Size(101, 23)
+        Me.pbrFileUploadProgress.TabIndex = 9
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Printer IP:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(114, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "COM Port:"
+        '
+        'btnOctoPrint
+        '
+        Me.btnOctoPrint.Location = New System.Drawing.Point(12, 80)
+        Me.btnOctoPrint.Name = "btnOctoPrint"
+        Me.btnOctoPrint.Size = New System.Drawing.Size(85, 23)
+        Me.btnOctoPrint.TabIndex = 15
+        Me.btnOctoPrint.Text = "Start OctoPrint"
+        Me.btnOctoPrint.UseVisualStyleBackColor = True
+        '
+        'btnYawam
+        '
+        Me.btnYawam.Location = New System.Drawing.Point(103, 80)
+        Me.btnYawam.Name = "btnYawam"
+        Me.btnYawam.Size = New System.Drawing.Size(87, 23)
+        Me.btnYawam.TabIndex = 16
+        Me.btnYawam.Text = "Start Yawcam"
+        Me.btnYawam.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(198, 80)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(60, 23)
+        Me.Button1.TabIndex = 17
+        Me.Button1.Text = "UI"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SettingsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(385, 24)
+        Me.MenuStrip1.TabIndex = 18
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'SettingsToolStripMenuItem
+        '
+        Me.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem"
+        Me.SettingsToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
+        Me.SettingsToolStripMenuItem.Text = "Settings"
+        '
+        'cbComPort
+        '
+        Me.cbComPort.FormattingEnabled = True
+        Me.cbComPort.Location = New System.Drawing.Point(117, 54)
+        Me.cbComPort.Name = "cbComPort"
+        Me.cbComPort.Size = New System.Drawing.Size(73, 21)
+        Me.cbComPort.TabIndex = 19
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(385, 217)
+        Me.Controls.Add(Me.cbComPort)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnYawam)
+        Me.Controls.Add(Me.btnOctoPrint)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.pbrFileUploadProgress)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.tbPrinterIP)
+        Me.Controls.Add(Me.tbLog)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "MPSM WIFI TUNNEL v0.1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tbLog As TextBox
+    Friend WithEvents tbPrinterIP As TextBox
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents pbrFileUploadProgress As ProgressBar
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnOctoPrint As Button
+    Friend WithEvents btnYawam As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents SettingsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents cbComPort As ComboBox
+End Class
---
+++ OctoPrint WiFi Tunnel/Form1.vb
@@ -0,0 +1,353 @@
+﻿Imports System.Threading
+Imports System.Text
+Imports System.IO.Ports
+Imports System.Net
+Imports System.Net.WebSockets
+Imports System.IO
+
+Public Class Form1
+    Public g_cts As New CancellationTokenSource()
+    Public g_socket As New ClientWebSocket()
+    Dim g_strUri As String
+    Dim g_SerialPort As SerialPort
+    Dim g_strComPort As String
+    Dim g_IsOctoPrintRunning = False
+    Dim g_IsYawCamRunning = False
+    Dim g_strPrintersIP As String
+    Dim g_IsUploadingFile = False
+    Private WithEvents UploadWebClient As New WebClient
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            'tbComPort.Text = My.Settings.ComPort
+            cbComPort.SelectedText = My.Settings.ComPort
+            tbPrinterIP.Text = My.Settings.PrinterIP
+            g_strPrintersIP = tbPrinterIP.Text
+            Timer1.Start()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
+    End Sub
+
+    Private Sub DataReceivedHandler(sender As Object, e As SerialDataReceivedEventArgs)
+        Try
+            If g_IsUploadingFile Then
+                Exit Sub
+            End If
+
+            Dim sp As SerialPort = CType(sender, SerialPort)
+            Dim data As String = sp.ReadLine
+            SendSocketCommand(data)
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+    Sub WorkerThread()
+        Try
+            Start().Wait()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Public Async Function Start() As Task
+        Try
+            g_socket = New ClientWebSocket()
+            AddLog("Connecting to printer...")
+
+            'socket.Options.KeepAliveInterval = New TimeSpan(90000)
+            Await g_socket.ConnectAsync(New Uri(g_strUri), g_cts.Token)
+
+            Dim strState As String = WebSocketState.Closed
+            If (g_socket.State = WebSocketState.Open) Then
+                strState = "Connected"
+            ElseIf (g_socket.State = WebSocketState.Connecting) Then
+                strState = "Connecting"
+            End If
+
+            Do While Not g_socket.State = WebSocketState.Open
+                AddLog("Connecting...")
+                Threading.Thread.Sleep(1000)
+                Application.DoEvents()
+            Loop
+
+            AddLog(strState)
+            AddLog("Waiting for connection from OctoPrint...")
+
+            Await Task.Factory.StartNew(
+                Async Function()
+                    Dim rcvBytes = New Byte(127) {}
+                    Dim rcvBuffer = New ArraySegment(Of Byte)(rcvBytes)
+                    While True
+                        Dim rcvResult As WebSocketReceiveResult = Await g_socket.ReceiveAsync(rcvBuffer, g_cts.Token)
+                        Dim msgBytes As Byte() = rcvBuffer.Skip(rcvBuffer.Offset).Take(rcvResult.Count).ToArray()
+                        Dim rcvMsg As String = Encoding.UTF8.GetString(msgBytes)
+                        AddLog("Received: " & rcvMsg)
+                        g_SerialPort.Write(rcvMsg)
+                    End While
+                End Function, g_cts.Token, TaskCreationOptions.LongRunning, TaskScheduler.[Default])
+        Catch ex As Exception
+            If g_socket IsNot Nothing Then
+                g_socket.Abort()
+                g_socket.Dispose()
+            End If
+
+            If g_SerialPort IsNot Nothing Then
+                g_SerialPort.Close()
+                g_SerialPort.Dispose()
+            End If
+            MsgBox("Connection Error: Is printer turned on and connected to wifi?" & vbNewLine & ex.ToString)
+        End Try
+    End Function
+
+    Public Sub SendSocketCommand(ByVal message As String)
+        Try
+            AddLog("Sent: " & message)
+            Dim sendBytes As Byte() = Encoding.ASCII.GetBytes(message)
+            Dim sendBuffer = New ArraySegment(Of Byte)(sendBytes)
+            g_socket.SendAsync(sendBuffer, WebSocketMessageType.Text, endOfMessage:=True, cancellationToken:=g_cts.Token).Wait()
+        Catch ex As Exception
+            AddLog("SendSocketCommand Error: " & ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub AddLog(message As String)
+        Try
+            If tbLog.InvokeRequired Then
+                tbLog.Invoke(DirectCast(AddressOf AddLog, Action(Of String)), message)
+                Return
+            End If
+            tbLog.AppendText(message & vbNewLine)
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub btnUpload_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        Try
+            Dim result As Integer = MessageBox.Show("Is the printer connected and octoprint disconnected?", "Warning!", MessageBoxButtons.YesNoCancel)
+            If result = DialogResult.Cancel Then
+                Exit Sub
+            ElseIf result = DialogResult.No Then
+                Exit Sub
+            ElseIf result = DialogResult.Yes Then
+                Try
+                    g_IsUploadingFile = True
+
+                    SendWebCommand("http://" & g_strPrintersIP & "/set?code=M563 S6")
+
+                    OpenFileDialog1.InitialDirectory = "c:\"
+                    OpenFileDialog1.Filter = "GCODE Files (*.gcode)|*.gc|All files (*.*)|*.*"
+                    OpenFileDialog1.FilterIndex = 2
+                    OpenFileDialog1.RestoreDirectory = True
+
+                    If OpenFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+                        Dim myUri As String = String.Format("http://" & g_strPrintersIP & "/upload")
+                        UploadWebClient.UploadFileTaskAsync(New Uri(myUri), OpenFileDialog1.FileName)
+                        'UploadWebClient.UploadFileAsync(New Uri(myUri), OpenFileDialog1.FileName)
+                    End If
+                Catch ex As Exception
+                    MsgBox(ex.ToString)
+                End Try
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
+    End Sub
+
+    Private Sub myFtpUploadWebClient_UploadFileCompleted(sender As Object, e As System.Net.UploadFileCompletedEventArgs) Handles UploadWebClient.UploadFileCompleted
+        Try
+            If e.Error IsNot Nothing Then
+                MsgBox("Error uploading file: " & e.Result.ToString & "-" & e.Error.Message)
+                pbrFileUploadProgress.Value = 0
+                g_IsUploadingFile = False
+                Exit Sub
+            End If
+
+            Dim result As Integer = MessageBox.Show("File upload complete!, Would you like to rename the file or leave it as cache.gc?", "Notice", MessageBoxButtons.YesNo)
+            If result = DialogResult.Yes Then
+                'SendSocketCommand("M566 " & OpenFileDialog1.SafeFileName)
+                SendWebCommand("http://" & g_strPrintersIP & "/set?code=M566 " & OpenFileDialog1.SafeFileName)
+                MsgBox("Ok, file was renamed to " & OpenFileDialog1.SafeFileName)
+            ElseIf result = DialogResult.No Then
+                MsgBox("Ok, filename left as cache.gc")
+            End If
+
+            pbrFileUploadProgress.Value = 0
+            g_IsUploadingFile = False
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub UploadWebClient_UploadProgressChanged(sender As Object, e As System.Net.UploadProgressChangedEventArgs) Handles UploadWebClient.UploadProgressChanged
+        Try
+            Dim total As Integer = e.TotalBytesToSend
+            Dim sent As Integer = e.BytesSent
+            Dim left As Integer = total - sent
+            pbrFileUploadProgress.Value = sent / total * 100
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+        Try
+
+            If g_SerialPort IsNot Nothing Then
+                g_SerialPort.Close()
+                g_SerialPort.Dispose()
+            End If
+
+            My.Settings.PrinterIP = tbPrinterIP.Text
+            My.Settings.ComPort = cbComPort.Text
+
+            g_strPrintersIP = tbPrinterIP.Text
+            g_strComPort = cbComPort.Text
+
+            g_strUri = String.Format("ws://" & g_strPrintersIP & ":81")
+            g_SerialPort = New SerialPort(g_strComPort)
+
+            Dim t As New Thread(AddressOf WorkerThread)
+            t.Start()
+
+            g_SerialPort.BaudRate = 115200
+            g_SerialPort.Parity = Parity.None
+            g_SerialPort.StopBits = StopBits.One
+            g_SerialPort.DataBits = 8
+            g_SerialPort.Handshake = Handshake.None
+            g_SerialPort.RtsEnable = True
+
+            AddHandler g_SerialPort.DataReceived, AddressOf DataReceivedHandler
+
+            AddLog("Opening Port")
+            g_SerialPort.Open()
+            AddLog("Port Open")
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub btnOctoPrint_Click(sender As Object, e As EventArgs) Handles btnOctoPrint.Click
+        Try
+            If g_IsOctoPrintRunning Then
+                For Each p As Process In Process.GetProcesses()
+                    If p.ProcessName.ToLower = "octoprint" Then
+                        p.Kill()
+                    End If
+                Next
+            Else
+                Process.Start("C:\OctoPrint\venv\Scripts\octoprint.exe", "serve")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
+    End Sub
+
+    Private Sub btnYawam_Click(sender As Object, e As EventArgs) Handles btnYawam.Click
+        Try
+            If g_IsYawCamRunning Then
+                For Each p As Process In Process.GetProcesses
+                    If p.ProcessName.ToLower = "javaw" Then
+                        p.Kill()
+                    End If
+                Next
+            Else
+                Dim p As New Process()
+                p.StartInfo.WorkingDirectory = "C:\Program Files (x86)\Yawcam"
+                p.StartInfo.FileName = "C:\Program Files (x86)\Yawcam\Yawcam.exe"
+                p.Start()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            g_IsYawCamRunning = False
+            g_IsOctoPrintRunning = False
+            For Each p As Process In Process.GetProcesses
+                If p.ProcessName.ToLower = "javaw" Then
+                    g_IsYawCamRunning = True
+                ElseIf p.ProcessName.ToLower = "octoprint" Then
+                    g_IsOctoPrintRunning = True
+                End If
+            Next
+
+            If g_IsYawCamRunning Then
+                btnYawam.Text = "Stop Yawcam"
+            Else
+                btnYawam.Text = "Start Yawcam"
+            End If
+
+            If g_IsOctoPrintRunning Then
+                btnOctoPrint.Text = "Stop Octoprint"
+            Else
+                btnOctoPrint.Text = "Start Octoprint"
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
+    End Sub
+
+    Public Function GetTime() As String
+        Try
+            Dim timeStamp As String = DateTime.Now.ToString("hhmmss")
+            timeStamp = timeStamp.Insert(2, ":")
+            timeStamp = timeStamp.Insert(5, ":")
+            Return timeStamp
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
+    End Function
+
+    Public Function SendWebCommand(ByVal cmd As String) As String
+        Try
+            Dim request As WebRequest = WebRequest.Create(cmd)
+            Dim response As WebResponse = request.GetResponse()
+            Dim strStatus As String = CType(response, HttpWebResponse).StatusDescription
+
+            Dim dataStream As Stream = response.GetResponseStream()
+            Dim reader As New StreamReader(dataStream)
+            Dim responseFromServer As String = reader.ReadToEnd()
+
+            AddLog(responseFromServer)
+            reader.Close()
+            response.Close()
+            Return responseFromServer
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+            Return "Error!"
+        End Try
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Process.Start("http://127.0.0.1:5000")
+        Catch ex As Exception
+            MsgBox(ex, ToString)
+        End Try
+    End Sub
+
+    Private Sub ComboBox1_DropDown(sender As Object, e As EventArgs) Handles cbComPort.DropDown
+        cbComPort.Items.Clear()
+
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            cbComPort.Items.Add(sp)
+        Next
+    End Sub
+End Class
+
+
+
+
+
+
